575eef33437b5e61380b6d85973df8bdb755fa72
Jason Rossi <rossijay@yahoo.com>
2022-02-17 16:54:55 -0800

    fixing indents.

--- app.py
+++ app.py
@@ -22,39 +22,38 @@ with open('sp_500_sectors_to_ticker.json') as json_file:
 sectors = list(sectors_to_tickers.keys())
 
 def execute(sector, beta, sharpe, roi):
-  """
-  This is the main data gathering for this app. It will call other functions
-  to assemble a main dataframe which can be used in different ways.
-  """
+    """
+    This is the main data gathering for this app. It will call other functions
+    to assemble a main dataframe which can be used in different ways.
+    """
 
-  # unpacking the lows and highs into variables that can be used more easliy.
-  beta_low, beta_high = beta
-  sharpe_low, sharpe_high = sharpe
-  roi_low, roi_high = beta
+    # unpacking the lows and highs into variables that can be used more easliy.
+    beta_low, beta_high = beta
+    sharpe_low, sharpe_high = sharpe
+    roi_low, roi_high = beta
 
-
-  main_df = get_sector_data(sector)
-  st.line_chart(main_df)  
+    main_df = get_sector_data(sector)
+    st.line_chart(main_df)  
 
 
 def get_sector_data(sector):
-  """
-  This function is responsible for loading all of the stock data within a sector.
-  """
+    """
+    This function is responsible for loading all of the stock data within a sector.
+    """
   
-  stocks_to_load = sectors_to_tickers[sector]
-  start = (pd.Timestamp.now() - pd.Timedelta(days=365)).isoformat()
-  end = pd.Timestamp.now().isoformat()
-        
-  main_df = alpaca.get_barset(stocks_to_load, start=start, end=end, timeframe='1D', limit=352).df
+    stocks_to_load = sectors_to_tickers[sector]
+    start = (pd.Timestamp.now() - pd.Timedelta(days=365)).isoformat()
+    end = pd.Timestamp.now().isoformat()
+  
+    main_df = alpaca.get_barset(stocks_to_load, start=start, end=end, timeframe='1D', limit=352).df
        
-  #dropping unused columns
-  main_df.drop(columns=['open','high','low','volume'], axis=1, level=1,inplace=True)
+    #dropping unused columns
+    main_df.drop(columns=['open','high','low','volume'], axis=1, level=1,inplace=True)
     
-  # the y axis is multi-dementional, and this is flattening it.
-  main_df.columns = [col[0] for col in main_df.columns.values]
+    # the y axis is multi-dementional, and this is flattening it.
+    main_df.columns = [col[0] for col in main_df.columns.values]
 
-  return main_df
+    return main_df
 
   
 
@@ -62,17 +61,17 @@ def get_sector_data(sector):
 
 def main():
 
-  # Title
-  st.title("Stock Selector App")
+    # Title
+    st.title("Stock Selector App")
 
-  #st.sidebar.title("Controls")
-  st.sidebar.info( "Select the criteria that you want here.")
-  sector = st.sidebar.selectbox("Sectors", sectors)
-  beta = st.sidebar.slider('Beta Range', 0.0, 5.0, (1.0,4.0))
-  sharpe = st.sidebar.slider('Sharpe Range', 0.0, 5.0, (1.0,4.0))
-  roi = st.sidebar.slider('ROI Range', 0.0, 5.0, (1.0,4.0))    
+    #st.sidebar.title("Controls")
+    st.sidebar.info( "Select the criteria that you want here.")
+    sector = st.sidebar.selectbox("Sectors", sectors)
+    beta = st.sidebar.slider('Beta Range', 0.0, 5.0, (1.0,4.0))
+    sharpe = st.sidebar.slider('Sharpe Range', 0.0, 5.0, (1.0,4.0))
+    roi = st.sidebar.slider('ROI Range', 0.0, 5.0, (1.0,4.0))    
   
-  execute(sector, beta, sharpe, roi)
+    execute(sector, beta, sharpe, roi)
   
 
 main()  
